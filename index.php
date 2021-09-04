5d3627049490f880e3375826bb323fb359bd2c12
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-09-05 00:11:20 +1200

    update index page

--- index.php
+++ index.php
@@ -6,9 +6,18 @@
 
             <p>
                 This database includes some of the most popular apps
-                available mobile devices. The <a
-                href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">
-                original dataset</a> can found at the preceding link.
+                available for mobile devices.
+                <h3>Acknowledgements & Sources</h3>
+                <ul>
+                    <li><a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">
+                    original "17K Mobile Strategy Games" dataset</a></li>
+                    <li>I used <a href="https://codepen.io/Bluetidepro/pen/GkpEa">this percentage based star rating</a>
+                        code, whose curators I thank for being able to recycle
+                        their code and use it for my project.
+                    </li>
+
+                </ul>
+
             </p>
 
 
