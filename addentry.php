759e344e36836522bf7784642aec9d301ac7b429
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-09-10 18:12:42 +1200

    Make add entry form live
    * import SQL from database
    * rename incorrectly named variables
    * add code to redirect to success page
    * add code to keep chosen genre chosen when form is submitted
    * add code to detect if developer is already present in database or not
    * add code to submit data to database
    * correctly indent all SQL code

--- addentry.php
+++ addentry.php
@@ -1,5 +1,9 @@
 <?php include("topbit.php");
 
+// Get Genre list from database
+$genre_sql="SELECT * FROM `genre` ORDER BY `genre`.`Genre` ASC";
+$genre_query=mysqli_query($dbconnect, $genre_sql);
+$genre_rs=mysqli_fetch_assoc($genre_query);
 
 // initialise form variables
 
@@ -12,15 +16,85 @@ $age = "";
 $rating = "";
 $rate_count = "";
 $cost = "";
-$inapp = 1;
+$in_app = 1;
 $description = "";
 
 $has_errors = "no";
 
 // code below execute when the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  // get values from form
+  $app_name = mysqli_real_escape_string($dbconnect, $_POST['app_name']);
+  $subtitle = mysqli_real_escape_string($dbconnect, $_POST['subtitle']);
+  $url = mysqli_real_escape_string($dbconnect, $_POST['url']);
+
+  $genreID = mysqli_real_escape_string($dbconnect, $_POST['genre']);
+
+  // if genreID is given, record value so it isn't lost if there are errors
+  if ($genreID != "") {
+    $genreitem_sql = "SELECT * FROM `genre` WHERE `GenreID` = $genreID";
+    $genreitem_query = mysqli_query($dbconnect, $genreitem_sql);
+    $genreitem_rs = mysqli_fetch_assoc($genreitem_query);
+
+    $genre = $genreitem_rs['Genre'];
+
+  } // end of genre-safekeeping if statement
+
+  $developer = mysqli_real_escape_string($dbconnect, $_POST['developer']);
+  $age = mysqli_real_escape_string($dbconnect, $_POST['age']);
+  $rating = mysqli_real_escape_string($dbconnect, $_POST['rating']);
+  $rate_count = mysqli_real_escape_string($dbconnect, $_POST['rating_count']);
+  $cost = mysqli_real_escape_string($dbconnect, $_POST['cost']);
+  $in_app = mysqli_real_escape_string($dbconnect, $_POST['in_app']);
+  $description = mysqli_real_escape_string($dbconnect, $_POST['description']);
+
+  // error checking will go here...
+
+  // if there are no errors...
+  if ($has_errors == "no") {
+
+    //  go to success page
+    header('Location: add_success.php');
+
+    // get developer id if it exits...
+    $developer_sql = "SELECT * FROM `developer` WHERE `Developer` LIKE '$developer'";
+    $developer_query = mysqli_query($dbconnect, $developer_sql);
+    $developer_rs = mysqli_fetch_assoc($developer_query);
+    $developer_count = mysqli_num_rows($developer_query);
+
+    // if developer not already in developer table, add them and get new developerID
+    if ($developer_count > 0) {
+      $developerID = $developer_rs['DeveloperID'];
+    }
+
+    else {
+      $add_developer_sql = "INSERT INTO `developer` (`DeveloperID`, `Developer`) VALUES (NULL, '$developer');";
+      $add_developer_query = mysqli_query($dbconnect, $add_developer_sql);
+
+      // Get developer ID
+      $newdeveloper_sql = "SELECT * FROM `developer` WHERE `Developer` LIKE '$developer'";
+      $newdeveloper_query = mysqli_query($dbconnect, $newdeveloper_sql);
+      $newdeveloper_rs = mysqli_fetch_assoc($newdeveloper_query);
+
+      $developerID = $newdeveloper_rs['DeveloperID'];
+
+    } // end adding developer to developer table
+
+    // add entry to database
+    $addentry_sql = "INSERT INTO `game_details` (`ID`, `Name`, `Subtitle`,
+      `URL`, `GenreID`, `DeveloperID`, `Age`, `User Rating`, `Rating Count`,
+       `Price`, `In App`, `Description`) VALUES (NULL, '$app_name', '$subtitle',
+          '$url', '$genreID', '$developerID', '$age', '$rating', '$rate_count',
+           '$cost', '$in_app', '$description');";
+    $addentry_query = mysqli_query($dbconnect, $addentry_sql);
+
+
+  } // end of 'no errors' if statement
+
   echo "You pushed the button";
-} // end of if statement
+
+} // end of if statement (executes when button is pushed)
 
  ?>
 
